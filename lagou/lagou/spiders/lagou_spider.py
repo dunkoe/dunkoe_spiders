61a9741446986597e7cd08ed1a0847b9b2f09eec
AlvinZhang86 <zhsh411@126.com>
2017-02-20 19:18:08 +0800

    rule

--- lagou/lagou/spiders/lagou_spider.py
+++ lagou/lagou/spiders/lagou_spider.py
@@ -19,17 +19,12 @@ class LagouSpider(DSpider):
     allowed_domains = ["lagou.com"]
     start_urls = (
         'https://www.lagou.com/',
-        # 'https://www.lagou.com/zhaopin/duomeitishejishi/',
-        # 'https://www.lagou.com/jobs/2769011.html',
     )
 
-    # def parse(self, response):
-    # self.parse_boot(response=response)
-    # self.parse_item(response=response)
-    # self.parse_detail(response=response)
     rules = (
         Rule(LinkExtractor(allow="/jobs/[0-9]+\.html"), callback='parse_3', follow=False),
-        Rule(LinkExtractor(allow="/zhaopin/.*/"), follow=True),
+        Rule(LinkExtractor(allow="/zhaopin/.*/.*"), follow=True),
+        # Rule(LinkExtractor(allow="/zhaopin/.*/[0-9]+/"), follow=True),
     )
 
     def parse_3(self, response):
